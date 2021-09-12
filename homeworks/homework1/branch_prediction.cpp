e496bfcc5a28e4f727e992c4190f09d81afc17f1
abhi1kumar <abhinav3663@gmail.com>
2021-09-12 18:11:13 -0400

    dynamic array, precision added homework1/branch_prediction.cpp

--- homeworks/homework1/branch_prediction.cpp
+++ homeworks/homework1/branch_prediction.cpp
@@ -1,17 +1,26 @@
+/*
+    Sample Run:
+    ./a.out sort array_size repeat
+    ./a.out 0  32768 1000
+*/
+
 #include <algorithm>
 #include <ctime>
 #include <iostream>
 #include <random>
+#include <iomanip>
 
 using namespace std;
-
 
 int main(int argc, char* argv[])
 {
     int seed             = 0;
     int MAX_VAL_IN_ARRAY = 256;
-    int REPEAT           = 1000;
+    int PRECISION        = 8;
+
     bool do_sort         = true;
+    unsigned array_size  = 32768;
+    int repeat           = 100000;
 
     std::mt19937_64 dre(seed);
     uniform_int_distribution<int> dist(0, MAX_VAL_IN_ARRAY-1);
@@ -19,8 +28,11 @@ int main(int argc, char* argv[])
     // =========================================================================
     // Command Line Arguments
     // =========================================================================
+    if (argc >= 4)
+        repeat = atoi(argv[3]);
+
     if (argc >= 3)
-        REPEAT = atoi(argv[2]);
+        array_size = atoi(argv[2]);
 
     if (argc >= 2)
     {        
@@ -29,37 +41,46 @@ int main(int argc, char* argv[])
         else
             do_sort = false;
     }
-    cout << "Sort= " << do_sort << "\t" << "Repeat= " << REPEAT << "\n";
 
     // =========================================================================
     // Generate data
     // =========================================================================
-    const unsigned arraySize = 32768;
-    int data[arraySize];
 
-    for (unsigned c = 0; c < arraySize; ++c)
+    //const unsigned array_size = 32768;
+    //int data[array_size];
+
+    // If we want a variable length or dynamically sized array in C++, we have to
+    // dynamically allocate memory ourself:
+    int *data = new int[array_size];
+
+    for (unsigned c = 0; c < array_size; ++c)
         //data[c] = rand() % MAX_VAL_IN_ARRAY;
         data[c] = dist(dre);
 
     if (do_sort)    
         // !!! With this, the next loop runs faster.
-        sort(data, data + arraySize);
+        sort(data, data + array_size);
 
     // =========================================================================
     // Time now
     // =========================================================================
     clock_t start = clock();
     long long sum = 0;
-    for (unsigned i = 0; i < REPEAT; ++i)
+    for (unsigned i = 0; i < repeat; ++i)
     {
-        for (unsigned c = 0; c < arraySize; ++c)
+        for (unsigned c = 0; c < array_size; ++c)
         {   // Primary loop
             if (data[c] >= 128)
                 sum += data[c];
         }
     }
 
+    // Delete the dynamic array
+    delete [] data;
+
     double elapsedTime = static_cast<double>(clock()-start) / CLOCKS_PER_SEC;
 
-    cout << "Sum= " << sum << " Elapsed Time: Avg= " << elapsedTime/REPEAT << " Total= " << elapsedTime << "\n";
+    cout << "Sort= " << do_sort << " Array_size = " << array_size << " " << "Repeat= " << repeat << " ";
+    cout.setf(ios::fixed);
+    cout << "Sum= " << sum << " Time_avg= " << setprecision(PRECISION) << elapsedTime/repeat << " Time_total= " << setprecision(PRECISION) << elapsedTime << "\n";
 }
