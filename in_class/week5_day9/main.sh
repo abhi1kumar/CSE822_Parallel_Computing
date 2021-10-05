f8362283e0a97d0a65cc058ca86b18a6ae72459b
abhi1kumar <abhinav3663@gmail.com>
2021-10-05 10:53:36 -0400

    in_class/week5_day9 added

--- in_class/week5_day9/main.sh
+++ in_class/week5_day9/main.sh
@@ -1,8 +1,15 @@
-g++ -pthread 3.1-async1.cpp -o 3.1-async1.o
-./3.1-async1.o
+g++ -pthread t-vector-2.cpp -o t-vector-2.o
+./t-vector-2.o 5
 
-g++ -pthread 3.2-async2.cpp -o 3.2-async2.o
-./3.2-async2.o
+#g++ -pthread -std=c++11 collatz.cpp -o collatz.o
+#printf "Running collatz...\n"
+#./collatz.o
 
-g++ -pthread 3.3-async3.cpp -o 3.3-async3.o
-./3.3-async3.o
+#g++ -pthread 3.1-async1.cpp -o 3.1-async1.o
+#./3.1-async1.o
+
+#g++ -pthread 3.2-async2.cpp -o 3.2-async2.o
+#./3.2-async2.o
+
+#g++ -pthread 3.3-async3.cpp -o 3.3-async3.o
+#./3.3-async3.o
