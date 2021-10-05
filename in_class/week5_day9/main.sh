717737c141c0a2ffa8d8e42cdb44c37d37a4cc4c
abhi1kumar <abhinav3663@gmail.com>
2021-10-05 10:33:18 -0400

    in_class/week5_day9 added

---
+++ in_class/week5_day9/main.sh
@@ -0,0 +1,8 @@
+g++ -pthread 3.1-async1.cpp -o 3.1-async1.o
+./3.1-async1.o
+
+g++ -pthread 3.2-async2.cpp -o 3.2-async2.o
+./3.2-async2.o
+
+g++ -pthread 3.3-async3.cpp -o 3.3-async3.o
+./3.3-async3.o
