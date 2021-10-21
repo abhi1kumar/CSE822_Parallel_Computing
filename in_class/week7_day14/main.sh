37e605808c4fca03c48499724858e606d2f9f891
abhi1kumar <abhinav3663@gmail.com>
2021-10-21 00:08:28 -0400

    in_class/week7_day14 added

---
+++ in_class/week7_day14/main.sh
@@ -0,0 +1,11 @@
+g++ func_sigs.cpp -o func_sigs.o
+./func_sigs.o
+
+g++ matching.cpp -o matching.o
+./matching.o
+
+g++ templateFunction.cpp -o templateFunction.o
+./templateFunction.o
+
+g++ forceTemplate.cpp -o forceTemplate.o
+./forceTemplate.o
